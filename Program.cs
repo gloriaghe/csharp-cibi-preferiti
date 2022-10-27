23a79f12edabab3316e634ab29aa721998ef3e86
gloriaghe <gloghe@yahoo.it>
2022-10-27 15:47:25 +0200

    Istruzioni condizionali fatte

--- Program.cs
+++ Program.cs
@@ -1,7 +1,44 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿//ESERCIZI POMERIGGIO
 
-using System;
-using System.Linq.Expressions;
+//Istruzioni condizionali
+//Snack1. L’utente inserisce due numeri in successione.
+//Il software stampa il maggiore.
+
+Console.WriteLine("Inserisci un numero");
+int primoNumero = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Inserisci un altro numero");
+int secondoNumero = Convert.ToInt32(Console.ReadLine());
+
+if(primoNumero > secondoNumero)
+{
+    Console.WriteLine(primoNumero);
+} else
+{
+    Console.WriteLine(secondoNumero);
+}
+
+//Snack2. L’utente inserisce due parole in successione.
+//Il software stampa prima la parola più corta, poi la parola più lunga.
+
+Console.WriteLine("Inserisci una parola");
+string primaParola = Console.ReadLine();
+
+Console.WriteLine("Inserisci un altra parola");
+string secondaParola = Console.ReadLine();
+
+if (primaParola.Length < secondaParola.Length)
+{
+    Console.WriteLine("La parola più corta è: " + primaParola + " mentre la più lunga è: " + secondaParola);
+}
+else
+{
+    Console.WriteLine("La parola più corta è: " + secondaParola + " mentre la più lunga è: " + primaParola);
+
+}
+
+
+//ESERCIZIO MATTINA
 
 //Create un progetto App Console csharp-cibi-preferiti con VS2022.
 //Nel programma inizializzate un array con la classifica dei vostri cibi preferiti (minimo 5, massimo 10 elementi).
@@ -16,45 +53,45 @@ using System.Linq.Expressions;
 //Attenzione: gestire anche il caso se aveste una classifica con un numero di elementi pari.
 //In questo caso vanno stampati i 2 elementi in centro alla vostra classifica.
 
-string[] cibiPreferiti = { "Pizza", "Carbonara", "Torta al cioccolato", "Lasagna", "Tagliata al sangue", "Tiramisù", "Fragole" , "Mango" };
+//string[] cibiPreferiti = { "Pizza", "Carbonara", "Torta al cioccolato", "Lasagna", "Tagliata al sangue", "Tiramisù", "Fragole" , "Mango" };
 
-Console.WriteLine("Lunghezza: " + cibiPreferiti.Length);
+//Console.WriteLine("Lunghezza: " + cibiPreferiti.Length);
 
-//altri modi per stampare:
-//Console.WriteLine("Lunghezza: {0}", cibiPreferiti.Length);
-//Console.WriteLine($"Lunghezza: {cibiPreferiti.Length}");
-
-
-Console.WriteLine();
-Console.WriteLine("Classifica cibi:");
-for (int i = 0; i < cibiPreferiti.Length; i++)
-{
-    Console.Write(i + 1 + " - "); //riga stampata che non va a capo
-    Console.WriteLine(cibiPreferiti[i]);
-
-    //Console.WriteLine(i+1 + cibiPreferiti[i]);
-
-}
+////altri modi per stampare:
+////Console.WriteLine("Lunghezza: {0}", cibiPreferiti.Length);
+////Console.WriteLine($"Lunghezza: {cibiPreferiti.Length}");
 
 
-Console.WriteLine();
+//Console.WriteLine();
+//Console.WriteLine("Classifica cibi:");
+//for (int i = 0; i < cibiPreferiti.Length; i++)
+//{
+//    Console.Write(i + 1 + " - "); //riga stampata che non va a capo
+//    Console.WriteLine(cibiPreferiti[i]);
 
-Console.WriteLine("Primo: " + cibiPreferiti[0]);
+//    //Console.WriteLine(i+1 + cibiPreferiti[i]);
 
-if(cibiPreferiti.Length % 2 == 0)
-{
-    //pari
-    int mediana = cibiPreferiti.Length / 2;
-    Console.WriteLine("Centrale: {0} e {1}", cibiPreferiti[mediana - 1], cibiPreferiti[mediana]);
-} else
-{
-    // dispari
-    int mediana = cibiPreferiti.Length / 2;
-    Console.WriteLine("Centrale: " + cibiPreferiti[mediana]);
+//}
 
 
-}
-Console.WriteLine("Ultimo: " + cibiPreferiti[cibiPreferiti.Length-1]);
+//Console.WriteLine();
+
+//Console.WriteLine("Primo: " + cibiPreferiti[0]);
+
+//if(cibiPreferiti.Length % 2 == 0)
+//{
+//    //pari
+//    int mediana = cibiPreferiti.Length / 2;
+//    Console.WriteLine("Centrale: {0} e {1}", cibiPreferiti[mediana - 1], cibiPreferiti[mediana]);
+//} else
+//{
+//    // dispari
+//    int mediana = cibiPreferiti.Length / 2;
+//    Console.WriteLine("Centrale: " + cibiPreferiti[mediana]);
+
+
+//}
+//Console.WriteLine("Ultimo: " + cibiPreferiti[cibiPreferiti.Length-1]);
 
 
 //for (int i = 0; i < cibiPreferiti.Length; i++)
@@ -73,6 +110,6 @@ Console.WriteLine("Ultimo: " + cibiPreferiti[cibiPreferiti.Length-1]);
 //        case 7:
 //            Console.WriteLine("Ultimo-" + cibiPreferiti[i]);
 //            break;
-       
+
 //    }
 //}
